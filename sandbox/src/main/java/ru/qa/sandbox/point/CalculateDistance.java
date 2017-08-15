1c5db924782f9a3aa28dcfb0c3a963532705b3ad
IsmagilovQA <ismagilov.vel@gmail.com>
2017-08-15 21:57:13 +0300

    Calculate distance - task #2

--- sandbox/src/main/java/ru/qa/sandbox/point/CalculateDistance.java
+++ sandbox/src/main/java/ru/qa/sandbox/point/CalculateDistance.java
@@ -6,9 +6,6 @@ public class CalculateDistance {
 
     Point p1 = new Point(4, 6);
     Point p2 = new Point(2, 8);
-    System.out.println("Distance between points is " + distance(p1, p2));
-  }
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
+    System.out.println("Distance between points is " + p2.distance(p1));
   }
 }
