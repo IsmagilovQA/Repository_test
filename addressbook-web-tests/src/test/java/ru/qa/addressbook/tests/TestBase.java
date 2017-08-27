58f7d15e2477563b60c51fbaa8544d84464f831f
IsmagilovQA <ismagilov.vel@gmail.com>
2017-08-27 22:30:20 +0300

    Run tests with FIREFOX, CHROME and IE browsers

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/TestBase.java
@@ -1,14 +1,13 @@
 package ru.qa.addressbook.tests;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.qa.addressbook.appmanager.ApplicationManager;
-import ru.qa.addressbook.model.ContactData;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
