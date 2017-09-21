a0e4873942f9bc8caa29d579df68a47ed030bb6e
IsmagilovQA <ismagilov.vel@gmail.com>
2017-09-21 23:16:26 +0300

    Parameterization of test methods with DataProvider (testNG)

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/ContactCreationTests.java
@@ -1,31 +1,40 @@
 package ru.qa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.qa.addressbook.model.ContactData;
 import ru.qa.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData().withFirstName("name 1").withMiddleName("middle name 1")
+            .withLastName("last name 1").withNickName("nickname 1").withCompany("Facebook")
+            .withAddress("NY, Baseinaja str 1, flat 1").withMobile("+380001234567").withGroup("name 1")});
+    list.add(new Object[]{new ContactData().withFirstName("name 2").withMiddleName("middle name 2")
+            .withLastName("last name 2").withNickName("nickname 2").withCompany("VK")
+            .withAddress("Boston, Baseinaja str 2, flat 2").withMobile("+380001234568").withGroup("name 2")});
+    list.add(new Object[]{new ContactData().withFirstName("name 3").withMiddleName("middle name 3")
+            .withLastName("last name 3").withNickName("nickname 3").withCompany("Fake")
+            .withAddress("LA, Baseinaja str 3, flat 3").withMobile("+380001234569").withGroup("name 3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     Contacts before = app.contact().all();
     app.goTo().contactPage();
     File photo = new File("src/test/resources/test_image.png");
-    ContactData contact = new ContactData()
-            .withFirstName("my name 1")
-            .withMiddleName("my middle name 1")
-            .withLastName("my last name 1")
-            .withNickName("my nickname 1")
-            .withCompany("Facebook")
-            .withAddress("New York, Baseinaja str 4, flat 7")
-            .withMobile("+380001234567")
-            .withGroup("name 1")
-            .withPhoto(photo);
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTests.java
@@ -1,8 +1,13 @@
 package ru.qa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.qa.addressbook.model.GroupData;
 import ru.qa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,11 +15,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("name 1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withName("name 2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withName("name 3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("name 1").withHeader("header 1").withFooter("footer 1");
     app.group().create(group);
     Groups after = app.group().all();
     assertThat(after.size(), equalTo(before.size() + 1));
